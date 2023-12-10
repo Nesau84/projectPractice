11c41fd702dbb50c7f54579bd90b5ac4e4e84a34
Milosh87 <53798476+Milosh87@users.noreply.github.com>
2023-12-10 20:03:32 +0100

    Reverse branch (#7)
    * Revert "Test (#6)"
    
    This reverts commit 011af0fbaffeeb4767b73747f4a3c74c6534b9cf.
    
    * reverse

--- src/test/java/enums/MainPageElements.java
+++ src/test/java/enums/MainPageElements.java
@@ -4,9 +4,7 @@ public enum MainPageElements {
 
 
     WHATS_NEW("What's New", "ui-id-3"),
-    WOMEN("Women", "ui-id-4"),
-    REVERT_TEST("revert_test", "u2-id-4"),
-    REVERT_TEST2("revert_test", "u2-id-4");
+    WOMEN("Women", "ui-id-4");
 
     private final String name;
     private final String pageElement;
